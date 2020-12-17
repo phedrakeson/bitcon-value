f8484ecf53d38c24eb387acea5271b6837d3bfb6
phedrakeson <gabrield.developer@gmail.com>
2020-12-17 16:00:11 -0300

    implementando função de horario e fetch api

---
+++ bitcoinValue.js
@@ -0,0 +1,15 @@
+function bitcoinValue() {
+  fetch('https://blockchain.info/ticker')
+  .then(r => r.json())
+  .then(bitcoin => {
+    const bitcoinBRL = bitcoin.BRL
+    const dateReturn = new Date;
+    document.querySelector('[data-buy]').innerText = bitcoinBRL.buy;
+    document.querySelector('[data-sell]').innerText = bitcoinBRL.sell;
+    document.querySelector('[data-time]').innerText = dateReturn.getHours() + ':' + dateReturn.getMinutes() + ':' + dateReturn.getSeconds();
+    setTimeout(bitcoinValue, 1000)
+  })
+}
+
+
+bitcoinValue();
